9e55f22b069178d18cbb13789c025b747c6468e3
AYU$H $HARMA <aayush240204@gmail.com>
2024-04-02 23:58:35 +0530

    Time: 15 ms (93.13%), Space: 27.3 MB (56.47%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,12 +1,28 @@
+// class Solution {
+// public:
+//     void rotate(vector<int>& nums, int k) {
+        
+//         vector<int> temp(nums.size());
+        
+//         for(int i = 0; i<nums.size();i++){
+//             temp[(i+k)%nums.size()] =  nums[i];
+//         }
+//         nums = temp;
+//     }
+// };
+
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k%n;
         
-        vector<int> temp(nums.size());
+        reverse(nums.begin(),nums.begin()+(nums.size()-k));
+
+        reverse(nums.begin()+(nums.size()-k),nums.end());
+    
+        reverse(nums.begin(),nums.end());
         
-        for(int i = 0; i<nums.size();i++){
-            temp[(i+k)%nums.size()] =  nums[i];
-        }
-        nums = temp;
+        
     }
 };
