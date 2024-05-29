577b779c4d65191adac6f6956918160908ab5b72
AYU$H $HARMA <aayush240204@gmail.com>
2024-05-29 14:28:03 +0530

    Time: 504 ms (93.48%), Space: 298.4 MB (5.47%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -11,28 +11,46 @@
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
+        // Bruteforce
+//         if(head->next == nullptr){
+//             return nullptr;
+//         }
+//         ListNode* temp = head;
+//         int n = 0;
+//         while(temp){
+//             n++;
+//             temp = temp->next;
+//         }
+
+//         int res = n/2;
+//         temp = head;
+//         while(temp){
+//             res--;
+//             if(res==0){
+//                 ListNode* middle = temp->next;
+//                 temp->next = temp->next->next;
+//                 delete middle;
+//                 break;
+//             }
+//             temp = temp->next;
+//         }
+//         return head;
+        
+        // Optimal
         if(head->next == nullptr){
             return nullptr;
         }
-        ListNode* temp = head;
-        int n = 0;
-        while(temp){
-            n++;
-            temp = temp->next;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        fast = fast->next->next; // skipping one iteration of slow by moving fast
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
         }
-
-        int res = n/2;
-        temp = head;
-        while(temp){
-            res--;
-            if(res==0){
-                ListNode* middle = temp->next;
-                temp->next = temp->next->next;
-                delete middle;
-                break;
-            }
-            temp = temp->next;
-        }
+        ListNode* middle = slow->next;
+        slow->next = slow->next->next;
+        delete middle;
         return head;
+        
     }
 };
