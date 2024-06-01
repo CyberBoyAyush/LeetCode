2548cf779ba552bb591647712ded77c1a86962b8
AYU$H $HARMA <aayush240204@gmail.com>
2024-06-01 22:50:32 +0530

    Time: 4 ms (48.99%), Space: 12.1 MB (7.54%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,9 +1,24 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        while(s.find(part)!=string::npos){
-            s.erase(s.find(part),part.length());
+        // while(s.find(part)!=string::npos){
+        //     s.erase(s.find(part),part.length());
+        // }
+        // return s;
+
+        // stack
+        int partLen = part.length();
+        string stack; // Using a string as a stack
+        
+        for (char c : s) {
+            stack.push_back(c); // Push character onto stack
+            
+            // Check if the top of the stack contains the substring `part`
+            if (stack.size() >= partLen && stack.substr(stack.size() - partLen) == part) {
+                stack.erase(stack.size() - partLen, partLen); // Remove the substring `part`
+            }
         }
-        return s;
+        
+        return stack; // The stack now contains the result string
     }
 };
