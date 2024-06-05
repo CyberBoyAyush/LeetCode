c4cb68b589e98ca82119837f566805772075d1de
AYU$H $HARMA <aayush240204@gmail.com>
2024-06-05 17:59:24 +0530

    Time: 4 ms (32.89%), Space: 7.3 MB (87.65%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.cpp
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -1,50 +1,6 @@
 class Solution {
 public:
     string reorganizeString(string str) {
-    //    int hash[26] = {0};
-    //    int n = s.length();
-
-    //     //storing freq
-    //    for(int i =0; i<n; i++){
-    //     hash[s[i] - 'a']++;
-    //    }
-
-    //     //find most freq char
-    //     char maxFreqChar;
-    //     int maxFreq = INT_MIN;
-    //     for(int i = 0; i<26; i++){
-    //         if(hash[i]>maxFreq){
-    //             maxFreq = hash[i];
-    //             maxFreqChar = i + 'a';
-    //         }
-    //     }
-
-    //     //now placing max freq char adjacently
-    //     int index = 0;
-    //     while(maxFreq>0 && index<n){
-    //         s[index] = maxFreqChar;
-    //         maxFreq--;
-    //         index+=2; //skips by 2
-    //     }
-
-    //     if(maxFreq != 0){
-    //         return "";
-    //     }
-
-    //     //now rmeoving freq of most frequent char
-    //     hash[maxFreqChar - 'a'] = 0;
-
-    //     // placing rest of char
-    //     for(int i = 0; i<n;i++){
-    //         while(hash[i]>0){
-    //             index = index>=n ? 1:index;
-    //             s[index] = i + 'a';
-    //             index+=2;
-    //             hash[i]--;
-                
-    //         }
-    //     }
-    //     return s;
     int hash[26] = {0};
         for (char c : str) {
             hash[c - 'a']++;
