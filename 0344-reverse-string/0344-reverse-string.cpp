4d0945e93ef95b36fc38c2090ad4a70e735aeab3
AYU$H $HARMA <aayush240204@gmail.com>
2024-06-10 18:23:29 +0530

    Time: 28 ms (6.88%), Space: 27.9 MB (21.08%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,10 +1,24 @@
 class Solution {
 public:
     void reverseString(vector<char>& arr) {
-        int s = 0;
-        int e = arr.size()-1;
-        while(s<e){
-        swap(arr[s++],arr[e--]);
-    }
+        // Two pointer -> best
+        // int s = 0;
+        // int e = arr.size()-1;
+        // while(s<e){
+        //     swap(arr[s++],arr[e--]);
+        // }
+
+        // using stack
+        stack<int> st;
+        for(auto it:arr){
+            st.push(it);
+        }
+
+        int i = 0;
+        while(!st.empty()){
+            arr[i] = st.top();
+            st.pop();
+            i++;
+        }
     }
 };
