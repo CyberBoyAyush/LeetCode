ea0f15c4adf8382c904348d84c4d4e7f797caddf
AYU$H $HARMA <aayush240204@gmail.com>
2024-06-11 18:33:05 +0530

    Time: 2 ms (46.59%), Space: 7.9 MB (14.47%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        int m = s.size();
+        int n = t.size();
+
+        stack<char> mp1 , mp2;
+        for(auto x:s){
+            if(x=='#' && !mp1.empty()){
+                mp1.pop();
+            }
+            else if(x=='#' && mp1.empty()) continue;
+            else mp1.push(x);
+        }
+        for(auto x:t){
+            if(x=='#' && !mp2.empty()){
+                mp2.pop();
+            }
+            else if(x=='#' && mp2.empty()) continue;
+            else mp2.push(x);
+        }
+
+        if(mp1 == mp2) return true;
+        else return false;
+    }
+};
