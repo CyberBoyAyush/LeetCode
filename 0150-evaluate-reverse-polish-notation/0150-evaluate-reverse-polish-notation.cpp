d43474f543980630081c3488ac69229ae9373aa1
Ayush Sharma <aayush240204@gmail.com>
2024-06-18 17:22:41 +0530

    Time: 11 ms (25.23%), Space: 15.4 MB (85.75%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,31 +1,64 @@
+// Stack Method
+
+// class Solution {
+// public:
+//     int operate(int a , int b, string token){
+//         if(token=="+")
+//             return a+b;
+//         else if(token == "-")
+//             return a-b;
+//         else if(token == "*")
+//             return (long) a * (long) b; // can overflow
+//         else
+//             return a/b;
+
+//         return -1;
+//     }
+//     int evalRPN(vector<string>& tokens) {
+//         stack<int> st;
+//         for(auto token : tokens){
+//             if(token == "+" || token == "-" || token == "*" || token == "/"){
+//                 int b = st.top();
+//                 st.pop();
+//                 int a = st.top();
+//                 st.pop();
+
+//                 int result = operate(a,b,token);
+//                 st.push(result);
+//             }
+//             else{
+//                 st.push(stoi(token)); // we convert in into integer
+//             }
+//         }
+//         return st.top();
+//     }
+// };
+
+// Lambda Method
 class Solution {
 public:
-    int operate(int a , int b, string token){
-        if(token=="+")
-            return a+b;
-        else if(token == "-")
-            return a-b;
-        else if(token == "*")
-            return (long) a * (long) b; // can overflow
-        else
-            return a/b;
-
-        return -1;
-    }
     int evalRPN(vector<string>& tokens) {
         stack<int> st;
-        for(auto token : tokens){
-            if(token == "+" || token == "-" || token == "*" || token == "/"){
+        int result = 0;
+        
+        unordered_map<string, function<int (int, int)> > mp = {
+            {"+", [](int a, int b) {return a + b; } },
+            {"-", [](int a, int b) {return a - b; } },
+            {"*", [](int a, int b) {return (long)a * (long)b; } },
+            {"/", [](int a, int b) {return a / b; } },
+        };
+        
+        for(const string& s:tokens) {
+            if(s == "+" || s == "-" || s == "*" || s == "/") {
                 int b = st.top();
                 st.pop();
                 int a = st.top();
                 st.pop();
-
-                int result = operate(a,b,token);
+                
+                result = mp[s](a, b);
                 st.push(result);
-            }
-            else{
-                st.push(stoi(token)); // we convert in into integer
+            } else {
+                st.push(stoi(s));
             }
         }
         return st.top();
