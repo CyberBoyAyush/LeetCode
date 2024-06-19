87dae5b011c8341cba07c0aa260dc1d6cd05984b
Ayush Sharma <aayush240204@gmail.com>
2024-06-19 21:43:16 +0530

    Time: 116 ms (42.23%), Space: 69 MB (65.37%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,51 +1,37 @@
 class Solution {
-    bool possible(vector<int>& bloomDay, int day, int m, int k){
-        int n = bloomDay.size();
-        int cnt = 0, noOfBouqets = 0;
-        for(int i = 0; i<n; i++){
-            if(bloomDay[i]<=day){
-                cnt ++;
+public:
+    int getNumOfB(vector<int>& bloomDay , int mid , int k){
+        int cnt = 0;
+        int noOfB = 0; // no of bouquets
+
+        for(int i = 0; i<bloomDay.size(); i++){
+            if(bloomDay[i]<=mid){
+                cnt++;
             }
             else{
-                noOfBouqets += cnt/k;
+                cnt = 0;
+            }
+            if(cnt==k){
+                noOfB++;
                 cnt = 0;
             }
         }
-        noOfBouqets += cnt/k;
-        //return noOfBouqets >= m;
-        if(noOfBouqets>=m){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return noOfB;
     }
-public:
     int minDays(vector<int>& bloomDay, int m, int k) {
-        // Edge case
-        long long val=m*1ll*k*1ll;
-        int n=bloomDay.size();
-        if(val>n){
-            return -1;
-        }
-        int low = INT_MAX , high = INT_MIN , ans = 0;
-        for(int i=0 ;i<n;i++){
-            low = min(bloomDay[i],low);
-            high = max(bloomDay[i],high);
-        }
-        if((m*k)>n){
-            return -1;
-        }
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(possible(bloomDay , mid, m, k)){
-                ans = mid;
-                high = mid -1; // we need minimum so we go to the left part of the array
+        int start = 0;
+        int end = *max_element(bloomDay.begin() , bloomDay.end() ); // maximum element present in the array bloom day
+        int minDays = -1;
+        while(start<=end){
+            int mid = start + (end-start) / 2;
+            if(getNumOfB(bloomDay,mid,k) >= m){
+                minDays = mid; // mid hi hamara minDays hai
+                end = mid - 1; // we will cheeck for any other minimum day possible
             }
             else{
-                low = mid + 1;
+                start = mid + 1;
             }
         }
-        return ans;
+        return minDays;
     }
 };
