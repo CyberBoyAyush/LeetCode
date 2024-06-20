01440bff2cd24174472e08fde2a78800d75817d8
Ayush Sharma <aayush240204@gmail.com>
2024-06-20 23:56:02 +0530

    Update 2073-time-needed-to-buy-tickets.cpp

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -2,27 +2,27 @@ class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
         // BruteForce [O(n*m)]
-        // queue<int> q;
-        // int n = tickets.size();
-        // // push all index in queue
-        // for(int i = 0; i<n; i++){
-        //     q.push(i);
-        // }
-        // // set time = 0
-        // int time = 0;
-        // // now do operation for ticket and time
-        // while(tickets[k]!=0){
-        //     tickets[q.front()]--;
-        //     time++;
-        //     if(tickets[q.front()]){ // agar q.front 0 nahi hai toh
-        //         q.push(q.front());
-        //         q.pop();
-        //     }
-        //     else{
-        //         q.pop();
-        //     }    
-        // }
-        // return time;
+        queue<int> q;
+        int n = tickets.size();
+        // push all index in queue
+        for(int i = 0; i<n; i++){
+            q.push(i);
+        }
+        // set time = 0
+        int time = 0;
+        // now do operation for ticket and time
+        while(tickets[k]!=0){
+            tickets[q.front()]--;
+            time++;
+            if(tickets[q.front()]){ // agar q.front 0 nahi hai toh
+                q.push(q.front());
+                q.pop();
+            }
+            else{
+                q.pop();
+            }    
+        }
+        return time;
         
         // Optimal [0(n)]
         int time = 0;
