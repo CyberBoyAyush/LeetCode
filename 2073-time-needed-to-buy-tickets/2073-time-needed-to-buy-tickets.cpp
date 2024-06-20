17bfd35b9ecd7b2e97e724a18186a95e0b7d37e7
Ayush Sharma <aayush240204@gmail.com>
2024-06-20 23:55:08 +0530

    Time: 2 ms (53.40%), Space: 9.3 MB (89.74%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -1,25 +1,39 @@
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
-        queue<int> q;
-        int n = tickets.size();
-        // push all index in queue
-        for(int i = 0; i<n; i++){
-            q.push(i);
-        }
-        // set time = 0
+        // BruteForce [O(n*m)]
+        // queue<int> q;
+        // int n = tickets.size();
+        // // push all index in queue
+        // for(int i = 0; i<n; i++){
+        //     q.push(i);
+        // }
+        // // set time = 0
+        // int time = 0;
+        // // now do operation for ticket and time
+        // while(tickets[k]!=0){
+        //     tickets[q.front()]--;
+        //     time++;
+        //     if(tickets[q.front()]){ // agar q.front 0 nahi hai toh
+        //         q.push(q.front());
+        //         q.pop();
+        //     }
+        //     else{
+        //         q.pop();
+        //     }    
+        // }
+        // return time;
+        
+        // Optimal [0(n)]
         int time = 0;
-        // now do operation for ticket and time
-        while(tickets[k]!=0){
-            tickets[q.front()]--;
-            time++;
-            if(tickets[q.front()]){ // agar q.front 0 nahi hai toh
-                q.push(q.front());
-                q.pop();
+        int n = tickets.size();
+        for(int i = 0; i<n ;i++){
+            if(i<=k){
+                time += min(tickets[i],tickets[k]);
             }
             else{
-                q.pop();
-            }    
+                time += min(tickets[k]-1 , tickets[i]);
+            }
         }
         return time;
     }
