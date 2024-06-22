e4f01a86f17097d8845fdde40e715ee6fbe88d5e
Ayush Sharma <aayush240204@gmail.com>
2024-06-23 02:28:35 +0530

    Time: 20 ms (73.27%), Space: 31 MB (85.99%) - LeetHub

---
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -0,0 +1,59 @@
+// Approach 1 : Preefix using using map
+
+// class Solution {
+// public:
+//     int numSubarraysWithSum(vector<int>& nums, int goal) {
+//         int n = nums.size();
+//         unordered_map<int,int> mpp;
+//         mpp[0] = 1;
+//         int currSum = 0;
+//         int res = 0;
+//         for (int i = 0; i<n ;i++){
+//             currSum += nums[i];
+
+//             if(mpp.find(currSum - goal) != mpp.end()){
+//                 res += mpp[currSum - goal];
+//             }
+
+//             mpp[currSum]++;
+
+//         }
+//         return res;
+//     }
+// };
+
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int n = nums.size();
+
+        int res = 0;
+        int cntZeros = 0;
+        int window_sum = 0;
+
+        int i = 0, j = 0;
+
+        while(j<n){
+            window_sum += nums[j];
+
+            while( i < j && (nums[i] == 0 || window_sum > goal)){
+                if(nums[i] == 0){ 
+                   cntZeros++; 
+                }
+                else{
+                    cntZeros = 0;
+                }
+
+                window_sum -= nums[i];
+                i++;
+            }
+
+            if(window_sum == goal){
+                res += 1 + cntZeros;
+            }
+
+            j++;
+        }
+        return res;
+    }
+};
