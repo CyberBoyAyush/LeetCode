34f13958e6ea55f119b57b875d2c5fe712b5ead7
Ayush Sharma <aayush240204@gmail.com>
2024-06-24 03:15:48 +0530

    Time: 3 ms (66.33%), Space: 11.9 MB (86.87%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,50 @@
+// Linear Search
+// TC - O(N*M)
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         int m = matrix.size();
+//         int n = matrix[0].size();
+
+//         for(int i = 0; i<m; i++){
+//             for(int j = 0 ; j<n ;j++){
+//                 if(matrix[i][j] == target){
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+// Binary Search
+// TC -O(log(n*m))
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int start = 0;
+        int end = n*m - 1;
+
+        while(start<=end){
+            int mid = start + (end-start) / 2;
+
+            // Converting 1D Indeexees to 2d indeexes for BS
+            int row = mid/n;
+            int col = mid%n;
+
+            if(matrix[row][col] == target){
+                return true;
+            }
+            else if(matrix[row][col] > target){
+                end = mid -1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return false;
+    }
+};
