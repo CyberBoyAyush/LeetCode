0797a385a207da2012b865d0990e47254406b632
Ayush Sharma <aayush240204@gmail.com>
2024-06-23 22:58:21 +0530

    Time: 30 ms (34.04%), Space: 94.7 MB (49.33%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -0,0 +1,120 @@
+// Bruteforce
+// TC - O(m+n) 
+// SC - O(m+n) 
+
+// class Solution {
+// public:
+//     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+//         int m = nums1.size();
+//         int n = nums2.size();
+
+//         // veector for storing merged arr
+//         vector<int> temp(m+n);
+
+//         int i = 0 , j=0, k=0; // 3 pointers for 3 arrays
+
+//         while(i<m && j<n){
+//             if(nums1[i] < nums2[j]){
+//                 temp[k++] = nums1[i++];
+//             }
+
+//             else{
+//                 temp[k++] = nums2[j++];
+//             }
+//         }
+
+//         // in case m and n are different size
+//         while(i<m){
+//             temp[k++] = nums1[i++];
+//         }
+
+//         while(j<n){
+//             temp[k++] = nums2[j++];
+//         }
+
+//         // now finding median
+//         int size = m + n;
+
+//         // for odd size
+//         if(size % 2 == 1){ // odd sizee
+//             return temp[size / 2];
+//         }
+        
+//         // for even
+//         return (temp[size / 2] + temp[size / 2 - 1]) / 2.0;
+//     }
+// };
+
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size();
+        int n = nums2.size();
+
+        int size = n + m;
+
+        int idx1 = (size/2) - 1;
+        int idx2 = size/2;
+
+        int el1 = -1, el2 = -1; // here we store out elments
+
+        int i = 0, j = 0 , k = 0;
+
+        while(i<m && j<n){
+            if(nums1[i] < nums2[j]){
+                if(k == idx1){
+                    el1 = nums1[i];
+                }
+
+                if(k == idx2){
+                    el2 = nums1[i];
+                }
+                i++;
+            }
+            else{
+                if(k == idx1){
+                    el1 = nums2[j];
+                }
+
+                if(k == idx2){
+                    el2 = nums2[j];
+                }
+                j++;
+            }
+            k++;
+        }
+
+        // for remaining one
+        while(i<m){
+            if(k == idx1){
+                    el1 = nums1[i];
+                }
+
+                if(k == idx2){
+                    el2 = nums1[i];
+                }
+                i++;
+                k++;
+        }
+
+        while(j<n){
+            if(k == idx1){
+                    el1 = nums2[j];
+                }
+
+                if(k == idx2){
+                    el2 = nums2[j];
+                }
+                j++;
+                k++;
+        }
+
+        // checking size for returning ans
+        if(size %2 == 1){ //odd
+            return el2;
+        }
+        return (el1 + el2) / 2.0;
+
+    }
+};
