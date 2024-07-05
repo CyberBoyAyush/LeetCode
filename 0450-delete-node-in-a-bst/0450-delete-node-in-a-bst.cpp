d4d2494b84e9a1b99e72b12266ec4b9fb1cbae79
Ayush Sharma <aayush240204@gmail.com>
2024-07-06 04:36:54 +0530

    Time: 32 ms (7.13%), Space: 32.1 MB (77.51%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -0,0 +1,83 @@
+/**
+ * Definition for a binary tree TreeNode.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int target) {
+        // Base Case
+    if(!root){
+        return NULL;
+    }
+    
+    //left
+    if(root->val > target){
+        root->left = deleteNode(root->left,target);
+        return root;
+    }
+    
+    // right
+    else if(root->val < target){
+        root->right = deleteNode(root->right,target);
+        return root;
+    }
+    
+    else{
+        // root->val == target
+        // TreeNode we delete is leaf TreeNode
+        if(!root->left && !root->right){
+            delete root;
+            return NULL;
+        }
+        
+        // TreeNode we delete has one child
+        else if(!root->left){
+            TreeNode* temp = root->right;
+            delete root;
+            return temp;
+        }
+        
+        else if(!root->right){
+            TreeNode* temp = root->left;
+            delete root;
+            return temp;
+        }
+        
+        // both child exists
+        else{
+            // find greater element from left or smaller from right we are finding greater from left
+            TreeNode* child = root->left;
+            TreeNode* parent = root;
+            
+            //right most 
+            while(child->right){
+                parent = child;
+                child = child->right;
+            }
+            
+            // case where root and parent is diff
+            if(root!=parent){
+                parent->right = child->left;
+                child->left = root->left;
+                child->right = root->right;
+                delete root;
+                return child;
+            }
+            
+            // other case
+            else{
+                child->right = root->right;
+                delete root;
+                return child;
+            }
+        }
+    }
+    }
+};
