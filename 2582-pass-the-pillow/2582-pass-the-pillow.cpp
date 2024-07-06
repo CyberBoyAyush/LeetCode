b6e5df566656b2a8c559c01f869f044b826ae2c3
Ayush Sharma <aayush240204@gmail.com>
2024-07-07 02:55:52 +0530

    Time: 3 ms (16.46%), Space: 7.2 MB (31.20%) - LeetHub

---
+++ 2582-pass-the-pillow/2582-pass-the-pillow.cpp
@@ -0,0 +1,38 @@
+// Simulation
+
+// class Solution {
+// public:
+//     int passThePillow(int n, int time) {
+//         int idx = 1;
+//         int dir = 1;
+//         while(time>0){
+//             if(idx + dir >= 1 && idx + dir <= n){
+//                 idx += dir;
+//                 time--;
+//             }
+//             else{
+//                 dir *= -1;
+//             }
+//         }
+//         return idx;
+//     }
+// };
+
+// Mathematics
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        int fullRounds = time / (n-1);
+        int timeLeft = time % (n-1);
+
+        if(fullRounds%2 == 0){
+            // even 
+            // means currently on one
+            return timeLeft +1;
+        }
+        else{
+            return n-timeLeft;
+        }
+    return -1;
+    }
+};
