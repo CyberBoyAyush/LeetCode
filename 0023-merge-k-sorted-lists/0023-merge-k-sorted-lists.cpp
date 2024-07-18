2c65ea4b196ee932b6d0854fc54d4dd6e13b3559
Ayush Sharma <aayush240204@gmail.com>
2024-07-18 18:01:19 +0530

    Time: 17 ms (51.73%), Space: 18.2 MB (13.36%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -1,37 +1,63 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+// class Solution {
+// public:
+//     ListNode* mergeKLists(vector<ListNode*>& lists) {
+//         priority_queue<pair<int,ListNode*> ,
+//         vector<pair<int,ListNode*>> , greater<pair<int,ListNode*>>> pq;
+//         for(int i = 0 ;i<lists.size();i++){
+//             if(lists[i]){
+//                 pq.push({lists[i]->val , lists[i]});
+//                 }
+//         }
+
+//         ListNode* dNode = new ListNode(-1);
+//         ListNode* temp = dNode;
+//         while(!pq.empty()){
+//             auto it = pq.top();
+//             pq.pop();
+//             if(it.second->next){
+//                 pq.push({it.second->next->val,it.second->next});
+//             }
+//             temp->next = it.second;
+//             temp = temp->next;
+//         }
+
+//         return dNode->next;
+
+//     }
+// };
+
 class Solution {
 public:
+    // custom comapre func
+    class Compare{
+        public:
+        bool operator()(ListNode* a, ListNode* b){
+            return a->val > b->val; // heap ke case me ulta hota h -> minheap
+        }
+    };
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<pair<int,ListNode*> ,
-        vector<pair<int,ListNode*>> , greater<pair<int,ListNode*>>> pq;
-        for(int i = 0 ;i<lists.size();i++){
-            if(lists[i]){
-                pq.push({lists[i]->val , lists[i]});
-                }
-        }
-
-        ListNode* dNode = new ListNode(-1);
-        ListNode* temp = dNode;
-        while(!pq.empty()){
-            auto it = pq.top();
-            pq.pop();
-            if(it.second->next){
-                pq.push({it.second->next->val,it.second->next});
+         // create a min heap
+        priority_queue<ListNode*, vector<ListNode*>, Compare> pq; // custom sort min heap
+        
+        for(auto node : lists){
+            if(node) {
+                pq.push(node);
             }
-            temp->next = it.second;
-            temp = temp->next;
         }
-
-        return dNode->next;
-
+        
+        // creating some pointer nodes
+        ListNode* dummyNode = new ListNode(-1);
+        ListNode* tail = dummyNode; // traverse in LL and points to tail
+        
+        ListNode* temp; // we push values from min heap in it
+        
+        while(!pq.empty()){
+            temp = pq.top();
+            pq.pop();
+            tail->next = temp;
+            tail = tail->next;
+            if(temp->next!=nullptr)  pq.push(temp->next);
+        }
+        return dummyNode->next;
     }
 };
