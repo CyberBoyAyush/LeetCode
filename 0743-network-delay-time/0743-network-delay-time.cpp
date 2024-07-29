f570a78c2311f43ce4865528b78daba92d655021
Ayush Sharma <aayush240204@gmail.com>
2024-07-29 22:24:28 +0530

    Time: 83 ms (59.09%), Space: 43.9 MB (60.82%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        // creationn of adj list
+        unordered_map<int,vector<pair<int,int>>> adj;
+        for(auto & vec : times){
+            int u = vec[0];
+            int v = vec[1];
+            int w = vec[2];
+
+            adj[u].push_back({v,w});
+        }
+
+        // min heap for Dijkstra's Algorithm
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+
+        vector<int> dist(n + 1,INT_MAX);
+
+        vector<int> Explored(n+1,0);
+
+        dist[k] = 0; // src node dist
+        
+        pq.push({0,k}); // pq initializiation
+
+        while(!pq.empty()){
+            int node = pq.top().second;
+            pq.pop();
+
+            if(Explored[node]==1)   continue;
+
+            Explored[node] = 1;
+
+            for(auto &v:adj[node]){
+                int neighbour = v.first;
+                int w = v.second;
+
+                if(!Explored[neighbour] && 
+                w + dist[node] < dist[neighbour]){
+                    dist[neighbour] = w + dist[node];
+                    pq.push({dist[neighbour] , neighbour});
+                }
+            }
+        }
+
+        int ans = INT_MIN;
+        
+        for(int i = 1; i <= n; i++)
+            ans = max(ans, dist[i]);
+        
+	    return ans == INT_MAX ? -1 : ans; 
+
+    }
+};
